aee19cfdf847d255a1d19dff799fed3835100903
Ezra Ramatong <128799204+ezra-ramatong@users.noreply.github.com>
2025-05-10 18:06:52 +0200

    tooling

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Some dope pizza yo",
+    }),
+    React.createElement(Pizza, {
+      name: "Americano Pizza",
+      description: "Frech fries and hotdogs",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian",
+      description: "Pineapple and ham",
+    }),
+    React.createElement(Pizza, {
+      name: "Chicken Pizza?",
+      description: "Sweet Chillin",
+    }),
+    React.createElement(Pizza, {
+      name: "Baked potato",
+      description: "Wtf?",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
